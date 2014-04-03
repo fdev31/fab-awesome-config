80f2350f7b5ab5a7171d07d426b1e79a21d27a7a
fab <fdevaux@wyplay.com>
2014-04-03 10:24:19 +0200

    remove useless screenlayout by default

--- rc.lua
+++ rc.lua
@@ -17,8 +17,6 @@ local awful = require("awful")
 awful.rules = require("awful.rules")
 require("awful.autofocus")
 
-awful.util.spawn_with_shell("sh ~/.screenlayout/good.sh")
-
 -- Widget and layout library
 local wibox = require("wibox")
 -- Theme handling library
