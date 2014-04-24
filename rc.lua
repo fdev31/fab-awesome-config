08bea8e2a29f66f58b9f7cf8f16f77d6352cd2ad
fab <fdev31@gmail.com>
2014-04-24 21:10:09 +0200

    remove double execution

--- rc.lua
+++ rc.lua
@@ -43,8 +43,6 @@ if awesome.startup_errors then
                      text = awesome.startup_errors })
 end
 
-os.execute("procs start")
-
 -- Handle runtime errors after startup
 do
     local in_error = false
