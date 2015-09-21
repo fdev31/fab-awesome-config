a9e48f3f7c0cb9a5b934abd889d4a55f5fd7c283
fab <fdev31@gmail.com>
2015-09-22 01:43:07 +0200

    refactor nic display so it can be used on startup

--- custom_menus.lua
+++ custom_menus.lua
@@ -5,29 +5,34 @@ require('custom_conf')
 
 netctl_menu = {}
 netctl = io.popen('netctl list')
+
+function nic_display()
+    local refresh_nic = timer({ timeout = 1 })
+    local refresh_limit = 30
+    refresh_nic:connect_signal("timeout", function ()
+        if nic or refresh_limit == 0 then
+            refresh_nic:stop()
+            if refresh_limit ~= 0 then
+                naughty.notify({title='Connected using '..nic})
+            end
+            return
+        end
+        set_nic()
+        refresh_limit = refresh_limit - 1
+    end)
+    refresh_nic:start()
+end
 for line in netctl:lines() do
     line = line:sub(3)
     table.insert( netctl_menu, { line, function()
         exec('sudo ifconfig w0 down ; sudo netctl stop-all && sudo netctl start '..line)
         nic = false
-        local refresh_nic = timer({ timeout = 1 })
-        local refresh_limit = 30
-        refresh_nic:connect_signal("timeout", function ()
-            if nic or refresh_limit == 0 then
-                refresh_nic:stop()
-                if refresh_limit ~= 0 then
-                    naughty.notify({title='Connected using '..nic})
-                end
-                return
-            end
-            set_nic()
-            refresh_limit = refresh_limit - 1
-        end)
-        refresh_nic:start()
+        nic_display()
     end
     } )
 end
 netctl:close()
+nic_display()
 
 -- Setup backlight object
 
