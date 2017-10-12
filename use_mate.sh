4753044dd48508547ce1b809bb22a95c16e22335
fab <fdev31@gmail.com>
2017-10-12 10:46:11 +0200

    add use_mate script

--- use_mate.sh
+++ use_mate.sh
@@ -1,3 +1,5 @@
+gsettings set org.gnome.desktop.interface scaling-factor 1  
+
 gsettings set org.mate.background show-desktop-icons false
 
 #gsettings set org.mate.session.required-components windowmanager awesome
