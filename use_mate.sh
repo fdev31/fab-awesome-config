bae935088916557403175578bfa079ef413ad4f3
fdev31 <fdev31@gmail.com>
2019-04-11 19:54:24 +0200

    update use-mate script to set the component list

--- use_mate.sh
+++ use_mate.sh
@@ -2,10 +2,8 @@ gsettings set org.gnome.desktop.interface scaling-factor 1
 
 gsettings set org.mate.background show-desktop-icons false
 
-#gsettings set org.mate.session.required-components windowmanager awesome
-
 gsettings set org.mate.session.required-components filemanager nautilus
 gsettings set org.mate.session.required-components windowmanager awesome
 gsettings set org.mate.session.required-components panel true
 
-
+gsettings set org.mate.session required-components-list "['windowmanager', 'panel', 'filemanager', 'dock']"
