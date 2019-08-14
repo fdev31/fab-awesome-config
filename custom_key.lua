25ed905da711128ef0c8bf944c2399567020a79c
fdev31 <fdev31@gmail.com>
2019-08-15 01:01:03 +0200

    Popup term is now borderless & bigger

--- custom_key.lua
+++ custom_key.lua
@@ -10,7 +10,7 @@ local menubar = require("menubar")
 local mixer = require('ponymix')
 require('my_conf')
 
-local dropShell = quake({app='kitty', name='DropTerm', argname='--name %s', followtag=true, width=0.95, height=0.4, vert='bottom', horiz='center'})
+local dropShell = quake({app='kitty', name='DropTerm', argname='--name %s', followtag=true, width=0.95, height=0.65, vert='bottom', horiz='center', border=0, overlap=true})
 
 local selected_layout = { }
 
